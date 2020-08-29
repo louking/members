louking/members

commit 3f3f3ccacd7343019602535cbba1322269bd0836

My Status Report / Meeting views: handle delete of discussion item / agenda item
* https://github.com/louking/members/issues/195

members/views/admin/meetings_admin.py
@@ -4,10 +4,10 @@ meetings_admin - administrative task handling for meetings admin
 '''
 # standard
 from uuid import uuid4
-from datetime import datetime
+from datetime import datetime, date
 
 # pypi
-from flask import g, request, url_for
+from flask import g, request
 from flask_security import current_user
 from dominate.tags import h1
 from sqlalchemy import func
@@ -765,10 +765,19 @@ motions.register()
 # meeting endpoint
 ###########################################################################################
 
-meeting_dbattrs = 'id,interest_id,meeting_id,order,title,agendaitem,discussion,is_attendee_only,is_action_only'.split(',')
-meeting_formfields = 'rowid,interest_id,meeting_id,order,title,agendaitem,discussion,is_attendee_only,is_action_only'.split(',')
+meeting_dbattrs = 'id,interest_id,meeting_id,order,title,agendaitem,discussion,is_attendee_only,is_action_only,is_hidden,hidden_reason'.split(',')
+meeting_formfields = 'rowid,interest_id,meeting_id,order,title,agendaitem,discussion,is_attendee_only,is_action_only,is_hidden,hidden_reason'.split(',')
 meeting_dbmapping = dict(zip(meeting_dbattrs, meeting_formfields))
 meeting_formmapping = dict(zip(meeting_formfields, meeting_dbattrs))
+
+def meeting_validate(action, formdata):
+    results = []
+
+    # if both of these are set, they will conflict with each other
+    if formdata['is_hidden'] == 'yes' and not formdata['hidden_reason']:
+        results.append({'name': 'hidden_reason', 'status': 'reason required when hiding agenda item'})
+
+    return results
 
 class MeetingView(DbCrudApiInterestsRolePermissions):
 
@@ -930,6 +939,15 @@ class MeetingView(DbCrudApiInterestsRolePermissions):
         super().beforequery()
         self.queryparams['meeting_id'] = request.args['meeting_id']
 
+        # should we display hidden agendaitems?
+        show_hidden = request.args.get('show_hidden', False)
+        if isinstance(show_hidden, str):
+            show_hidden = show_hidden != 'false'
+        if show_hidden:
+            self.queryparams.pop('is_hidden', None)
+        else:
+            self.queryparams['is_hidden'] = False
+
     def updatetables(self, rows):
         # todo: can this be part of configuration, or method to call per row or for all rows?
         for row in rows:
@@ -1019,6 +1037,36 @@ class MeetingView(DbCrudApiInterestsRolePermissions):
         output = super().createrow(formdata)
         return output
 
+    def deleterow(self, thisid):
+        """
+        check deletion is allowed
+
+        :param thisid: id for row
+        :return: empty list
+        """
+        agendaitem = AgendaItem.query.filter_by(id=thisid).one()
+
+        # no deletions allowed after meeting
+        today = date.today()
+        if agendaitem.meeting.date < today:
+            self._error = 'Cannot delete agenda item after meeting is over'
+            raise ParameterError(self._error)
+
+        # cannot delete an agenda item which came from a status report
+        if agendaitem.statusreport:
+            self._error = 'Cannot delete agenda item which came from a status report. Edit and set \'hide\' to \'yes\' instead'
+            raise ParameterError(self._error)
+
+        return super().deleterow(thisid)
+
+
+# hide / show hidden rows
+from dominate.tags import div, label, input
+hiddenfilter = div(_class='checkbox-filter FloatRight')
+with hiddenfilter:
+    input(type='checkbox', id='show-hidden-status', name='show-hidden-status', value='show-hidden')
+    label('Show hidden items', _for='show-hidden-status')
+
 # for parent / child editing see
 #   https://datatables.net/blog/2019-01-11#DataTables-Javascript
 #   http://live.datatables.net/bihawepu/1/edit
@@ -1039,6 +1087,8 @@ meeting = MeetingView(
     formmapping=meeting_formmapping,
     checkrequired=True,
     tableidtemplate='meeting-{{ meeting_id }}-{{ agendaitem_id }}',
+    pretablehtml=hiddenfilter.render(),
+    validate=meeting_validate,
     clientcolumns=[
         {'data':None,
          'name':'details-control',
@@ -1084,6 +1134,14 @@ meeting = MeetingView(
              'toolbar': ["heading", "|", "bold", "italic", "link", "bulletedList", "numberedList",
                          "|", "indent", "outdent", "|", "blockQuote", "insertTable", "undo", "redo"]
          }
+         },
+        {'data': 'is_hidden', 'name': 'is_hidden', 'label': 'Hide',
+         '_treatment': {'boolean': {'formfield': 'is_hidden', 'dbfield': 'is_hidden'}},
+         'visible': False,
+         },
+        {'data': 'hidden_reason', 'name': 'hidden_reason', 'label': 'Reason for Hiding',
+         'type': 'textarea',
+         'visible': False,
          },
     ],
     childrowoptions= {

members/model.py
@@ -406,7 +406,7 @@ class MemberStatusReport(Base):
         'version_id_col': version_id
     }
 
-# also "discussion point"
+# also accessed from "DiscussionItem" > "StatusReport"
 class AgendaItem(Base):
     __tablename__ = 'agendaitem'
     id = Column(Integer(), primary_key=True)
@@ -422,6 +422,8 @@ class AgendaItem(Base):
     is_action_only      = Column(Boolean, nullable=False, default=False)
     statusreport_id     = Column(Integer, ForeignKey('statusreport.id'))
     statusreport        = relationship('StatusReport', backref=backref('agendaitems'))
+    is_hidden           = Column(Boolean, nullable=False, default=False)
+    hidden_reason       = Column(Text)
     version_id = Column(Integer, nullable=False, default=1)
     __mapper_args__ = {
         'version_id_col': version_id

members/static/admin/afterdatatables.js
@@ -235,6 +235,45 @@ function afterdatatables() {
         // need on 'preInvites' to translate interest for Send Invites button
         editor.on( 'preInvites', translate_editor_group );
 
+        // only show hidden_reason field if is_hidden is true (yes)
+        editor.dependent('is_hidden', function(val, data, callback) {
+            // // the below doesn't work, and it might be confusing anyway
+            // var that = this;
+            // var elements = $( that.ids(true)[0] );
+            // if (val === 'no') {
+            //     elements.removeClass('hidden-row');
+            // } else {
+            //     elements.addClass('hidden-row');
+            // }
+            return val === 'no' ?
+                { hide: 'hidden_reason' } :
+                { show: 'hidden_reason' }
+        });
+
+        /**
+         * url hook gets called from within groups.js translate_datatable_group() anon fn
+         *
+         * @param url
+         * @returns {string}
+         */
+        function filter_hidden(url) {
+            // split out query parameters, and collect in object
+            var urlsplit = url.split('?');
+            var urlparams = allUrlParams();
+
+            // remove show_hidden if not needed, else set appropriately
+            delete urlparams['show_hidden']
+            if ($('#show-hidden-status').prop('checked') == true) {
+                urlparams['show_hidden'] = true;
+            }
+            url = urlsplit[0] + '?' + setParams(urlparams);
+            return url;
+        }
+        dt_add_url_hook(filter_hidden);
+        $('#show-hidden-status').change(function() {
+            _dt_table.draw();
+        });
+
     // special processing for history
     } else if (location.pathname.includes('/memberstatusreport')) {
         // for create show all fields except rsvp response

members/static/admin/nunjucks/meeting-child-row.js
@@ -15,6 +15,13 @@ output += "\r\n";
 }
 else {
 output += "\r\n    <div class=\"childrow-display\">\r\n        ";
+if(runtime.contextOrFrameLookup(context, frame, "is_hidden") == "yes" && runtime.contextOrFrameLookup(context, frame, "hidden_reason")) {
+output += "\r\n            <div class=\"DTE_Label\">HIDDEN: Reason for Hiding</div>\r\n            <div class=\"DTE_Field_Input\"><p>";
+output += runtime.suppressValue(env.getFilter("safe").call(context, runtime.contextOrFrameLookup(context, frame, "hidden_reason")), env.opts.autoescape);
+output += "</p></div>\r\n        ";
+;
+}
+output += "\r\n        ";
 if(runtime.contextOrFrameLookup(context, frame, "agendaitem")) {
 output += "\r\n            <div class=\"DTE_Label\">Summary</div>\r\n            <div class=\"DTE_Field_Input\">";
 output += runtime.suppressValue(env.getFilter("safe").call(context, runtime.contextOrFrameLookup(context, frame, "agendaitem")), env.opts.autoescape);

members/static/admin/templates/meeting-child-row.njk
@@ -7,6 +7,10 @@
 {# row just needs to be displayed #}
 {% else %}
     <div class="childrow-display">
+        {% if is_hidden == 'yes' and hidden_reason %}
+            <div class="DTE_Label">HIDDEN: Reason for Hiding</div>
+            <div class="DTE_Field_Input"><p>{{ hidden_reason | safe }}</p></div>
+        {% endif %}
         {% if agendaitem %}
             <div class="DTE_Label">Summary</div>
             <div class="DTE_Field_Input">{{ agendaitem | safe }}</div>

members/views/admin/meetings_member.py
@@ -4,6 +4,7 @@ meetings_member - handling for meetings member
 '''
 
 # standard
+from datetime import date
 
 # pypi
 from flask import request, flash, g
@@ -95,6 +96,21 @@ class MemberDiscussionsView(DbCrudApiInterestsRolePermissions):
         # prepare response
         thisrow = self.dte.get_response_data(dbrow)
         return thisrow
+
+    def deleterow(self, thisid):
+        """
+        check if date for meeting has passed before allowing discussion deletion
+
+        :param thisid: id for row
+        :return: empty list
+        """
+        discussionitem = DiscussionItem.query.filter_by(id=thisid).one()
+        today = date.today()
+        if discussionitem.meeting.date < today:
+            self._error = 'Cannot delete discussion item after meeting is over'
+            raise ParameterError(self._error)
+
+        return super().deleterow(thisid)
 
 memberdiscussions = MemberDiscussionsView(
     roles_accepted=[ROLE_SUPER_ADMIN, ROLE_MEETINGS_ADMIN, ROLE_MEETINGS_MEMBER],

migrations/versions/128c7e32169e_agendaitem_add_is_hidden_hidden_reason.py
@@ -1,0 +1,54 @@
+"""agendaitem: add is_hidden, hidden_reason
+
+Revision ID: 128c7e32169e
+Revises: f9160c45895b
+Create Date: 2020-08-26 13:51:07.531558
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '128c7e32169e'
+down_revision = 'f9160c45895b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade(engine_name):
+    globals()["upgrade_%s" % engine_name]()
+
+
+def downgrade(engine_name):
+    globals()["downgrade_%s" % engine_name]()
+
+
+
+
+
+def upgrade_():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.add_column('agendaitem', sa.Column('hidden_reason', sa.Text(), nullable=True))
+    op.add_column('agendaitem', sa.Column('is_hidden', sa.Boolean(), nullable=False))
+    # ### end Alembic commands ###
+
+
+def downgrade_():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_column('agendaitem', 'is_hidden')
+    op.drop_column('agendaitem', 'hidden_reason')
+    # ### end Alembic commands ###
+
+
+def upgrade_users():
+    # ### commands auto generated by Alembic - please adjust! ###
+    pass
+    # ### end Alembic commands ###
+
+
+def downgrade_users():
+    # ### commands auto generated by Alembic - please adjust! ###
+    pass
+    # ### end Alembic commands ###
+

commit f7fda90c915c712174b398b5b359ab4b441b1ef7

Meeting view: now displays purpose and date header (again)
* regression in https://github.com/louking/members/commit/3f3f3ccacd7343019602535cbba1322269bd0836
* https://github.com/louking/members/issues/198

members/views/admin/meetings_admin.py
@@ -9,7 +9,7 @@ from datetime import datetime, date
 # pypi
 from flask import g, request
 from flask_security import current_user
-from dominate.tags import h1
+from dominate.tags import h1, div, label, input
 from sqlalchemy import func
 from jinja2 import Template
 
@@ -807,8 +807,18 @@ class MeetingView(DbCrudApiInterestsRolePermissions):
     def format_pretablehtml(self):
         meetingid = request.args['meeting_id']
         meeting = Meeting.query.filter_by(id=meetingid, interest_id=localinterest().id).one_or_none()
-        html = h1('{} - {}'.format(meeting.date, meeting.purpose), _class='TextCenter')
-        return html.render()
+        pretablehtml = div()
+        with pretablehtml:
+            # meeting header
+            h1('{} - {}'.format(meeting.date, meeting.purpose), _class='TextCenter')
+
+            # hide / show hidden rows
+            hiddenfilter = div(_class='checkbox-filter FloatRight')
+            with hiddenfilter:
+                input(type='checkbox', id='show-hidden-status', name='show-hidden-status', value='show-hidden')
+                label('Show hidden items', _for='show-hidden-status')
+
+        return pretablehtml.render()
 
     def updateinvites(self):
         invites = Invite.query.filter_by(**self.queryparams).all()
@@ -1062,13 +1072,6 @@ class MeetingView(DbCrudApiInterestsRolePermissions):
         return super().deleterow(thisid)
 
 
-# hide / show hidden rows
-from dominate.tags import div, label, input
-hiddenfilter = div(_class='checkbox-filter FloatRight')
-with hiddenfilter:
-    input(type='checkbox', id='show-hidden-status', name='show-hidden-status', value='show-hidden')
-    label('Show hidden items', _for='show-hidden-status')
-
 # for parent / child editing see
 #   https://datatables.net/blog/2019-01-11#DataTables-Javascript
 #   http://live.datatables.net/bihawepu/1/edit
@@ -1089,7 +1092,6 @@ meeting = MeetingView(
     formmapping=meeting_formmapping,
     checkrequired=True,
     tableidtemplate='meeting-{{ meeting_id }}-{{ agendaitem_id }}',
-    pretablehtml=hiddenfilter.render(),
     validate=meeting_validate,
     clientcolumns=[
         {'data':None,