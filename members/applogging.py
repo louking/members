7a4282ccefb70550e623582879e294cc6a75a643
Lou King <lking@pobox.com>
2021-10-31 06:00:26 -0400

    applogging: timenow() is available for logpoints

--- members/applogging.py
+++ members/applogging.py
@@ -2,5 +2,16 @@
 applogging - define logging for the application
 ================================================
 '''
+# standard
+from datetime import datetime
+
+# pypi
+from loutilities.timeu import asctime
+
 # pick up common setlogging function (backwards compatibility)
 from loutilities.user.applogging import setlogging
+
+
+def timenow():
+    """useful for logpoints"""
+    return asctime('%H:%M:%S.%f').dt2asc(datetime.now())
