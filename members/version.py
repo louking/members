83d86c72a57551f08e0ae8084119885167c72202
Lou King <lking@pobox.com>
2024-05-05 07:26:18 -0400

    version 1.6.5

--- members/version.py
+++ members/version.py
@@ -1,5 +1,5 @@
 # See https://packaging.python.org/guides/distributing-packages-using-setuptools/#semantic-versioning-preferred
-__version__ = '1.6.5.dev1'
+__version__ = '1.6.5'
 __docversion__ = __version__
 # uncomment for development
 # __docversion__ = 'latest'
