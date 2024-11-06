06898a185ddb0239399631d34eb57f7943af9e4b
Lou King <lking@pobox.com>
2024-11-06 13:46:30 -0500

    Docker related changes, deployment (#591)

--- app/src/members/version.py
+++ app/src/members/version.py
@@ -1,5 +1,6 @@
-# See https://packaging.python.org/guides/distributing-packages-using-setuptools/#semantic-versioning-preferred
-__version__ = '1.6.7'
+from os import environ
+
+__version__ = environ['APP_VER']    # from .env
 __docversion__ = __version__
 # uncomment for development
 # __docversion__ = 'latest'
