a29079beaef9c48d90dbf5f6c34f07b75c3ecd1e
Lou King <lking@pobox.com>
2026-07-15 14:00:25 -0400

    show latest docs when dev version deployed

--- app/src/members/version.py
+++ app/src/members/version.py
@@ -1,6 +1,5 @@
 from os import environ
+from re import fullmatch
 
 __version__ = environ['APP_VER']    # from .env
-__docversion__ = __version__
-# uncomment for development
-# __docversion__ = 'latest'
+__docversion__ = __version__ if fullmatch(r'\d+\.\d+\.\d+', __version__) else 'latest'
